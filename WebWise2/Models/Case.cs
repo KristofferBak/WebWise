eb89250976d650b920fe6ecf4b92fd88f2786bc0
Kristoffer Bak <krwb@netcompany.com>
2020-08-05 11:35:43 +0200

    Case model

---
+++ WebWise2/Models/Case.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebWise2.Models
+{
+    public class Case
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+
+    }
+}
